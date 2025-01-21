55b16113679bde68b6642bc3c9ba523d2404e787
Esmeralda Nurce <EsmNur18@htl-shkoder.com>
2025-01-21 10:30:26 +0100

    test

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,22 +1,56 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    @org.junit.jupiter.api.Test
+    private Calculator calculator;
+
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
     void add() {
+        assertEquals(8, calculator.add(3,5));
+        assertNotEquals(8, calculator.add(3,4));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void subtract() {
+        assertEquals(-8, calculator.subtract(10, 18));
+        assertNotEquals(10, calculator.subtract(2,3) );
     }
 
-    @org.junit.jupiter.api.Test
-    void divide() {
+    @Test
+    void divide()  {
+        try {
+            assertEquals(5, calculator.divide(20, 4));
+        } catch (DivideByZeroException e) {
+            throw new RuntimeException();
+        }
+
+        try {
+            assertNotEquals(3, calculator.divide(4,1));
+        } catch (DivideByZeroException e) {
+            throw new RuntimeException();
+        }
+
+        assertThrows(DivideByZeroException.class, () -> calculator.divide(12, 0));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void multiply() {
+        assertEquals(12, calculator.multiply(3,4));
+        assertNotEquals(12, calculator.multiply(3,3));
     }
+
+
+
+
 }
